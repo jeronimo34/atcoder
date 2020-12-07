e4af79aba03260ec52eac9280fc0a6b1bd2080be
honma <unity1017@gmail.com>
2020-12-08 01:09:24 +0900

    abc177d, atc001b

---
+++ abc177d/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace abc177d
+{
+    class Program
+    {
+        static bool[] visited;
+        static List<List<int>> G;
+        static int res = 0;
+        static int cnt = 0;
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            var N = inputs[0];
+            var M = inputs[1];
+
+
+            G = new List<List<int>>();
+            for (int i = 0; i < N; ++i) G.Add(new List<int>());
+
+            visited = new bool[N];
+
+            for (var i = 0; i < M; ++i)
+            {
+                inputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)-1).ToArray();
+                var a = inputs[0];
+                var b = inputs[1];
+                G[a].Add(b);
+                G[b].Add(a);
+            }
+
+            for (int v = 0; v < N; ++v)
+            {
+                if (visited[v]) continue;
+                cnt = 0;
+                Dfs(v);
+                res = Math.Max(res, cnt);
+            }
+
+            Console.WriteLine(res);
+        }
+
+        static void Dfs(int v) 
+        {
+            visited[v] = true;
+            cnt++;
+
+            foreach (var nv in G[v]) {
+                if (visited[nv]) continue;
+                Dfs(nv);
+            }
+        }
+    }
+}
---
+++ atc001b/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace atc001b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            var N = inputs[0];
+            var Q = inputs[1];
+            UnionFind uf = new UnionFind(N);
+
+            for (int i = 0; i < Q; ++i) {
+                inputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                var (p, a, b) = (inputs[0], inputs[1], inputs[2]);
+
+                if (p == 0)
+                {
+                    //連結
+                    uf.Unite(a, b);
+                }
+                else 
+                {
+                    //判定
+                    if (uf.Same(a, b))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else {
+                        Console.WriteLine("No");
+                    }
+                }
+            }
+
+        }
+
+        class UnionFind {
+            int[] parents;
+            int[] ranks;
+
+            public UnionFind(int n) 
+            {
+                parents = new int[n];
+                ranks = new int[n];
+                for (int i = 0; i < n; ++i) {
+                    parents[i] = i;
+                    ranks[i] = 0;
+                }
+            }
+
+            private int Root(int x) {
+                return parents[x] == x ? x : parents[x] = Root(parents[x]);//経路圧縮
+            }
+
+            /// <summary>
+            /// 同じグループに属するか否か
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public bool Same(int x, int y) {
+                return Root(x) == Root(y);
+            }
+
+            /// <summary>
+            /// 併合
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            public void Unite(int x, int y) {
+                x = Root(x);
+                y = Root(y);
+                if (x == y) return;
+
+                if (ranks[x] < ranks[y])
+                {
+                    parents[x] = y;
+                }
+                else {
+                    parents[y] = x;
+                    if (ranks[x] == ranks[y]) ranks[x]++;
+                }
+            }
+        }
+    }
+}
